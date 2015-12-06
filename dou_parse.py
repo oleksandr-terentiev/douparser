14ccc49da4580facaeafa9dcc5ae2f6690083097
oleksandr-terentiev <oleksandr.terentiev@gmail.com>
2015-12-06 18:21:30 +0200

    Added more cities and interactive choosing.

--- dou_parse.py
+++ dou_parse.py
@@ -3,15 +3,11 @@ from lxml import etree
 from lxml import html
 from openpyxl import Workbook
 from datetime import datetime
-
+import csv
 
 class dou_parse:
     
-    city = ''
-    citylist = {'harkov': 'Харьков',
-                  'kiev': 'Киев'}
-                  
-    def __init__(self, city = 'harkov'):
+    def __init__(self, city = {'en': 'harkov', 'ru': 'Харьков'}):
         self.city = city
         
     def geturls(self, xmlfile):
@@ -27,7 +23,7 @@ class dou_parse:
             print 'Bad URL: ', url
             return ['Bad URL', '---', '---', '---', '---', url]
         offices = company.xpath("//div[@class='offices']/text()")
-        if len(offices) > 0 and self.citylist[self.city].decode('utf-8') in offices[0]:
+        if len(offices) > 0 and self.city['ru'].decode('utf-8') in offices[0]:
             offices = offices[0].strip()
         else:
             return None
@@ -43,7 +39,7 @@ class dou_parse:
         else:
             site = ''
         companyoffice = html.parse(url+'offices/')
-        adress = companyoffice.xpath("//a[@name='"+self.city+"']/../div/div[2]/div[1]/div/div[1]/text()")
+        adress = companyoffice.xpath("//a[@name='"+self.city['en']+"']/../div/div[2]/div[1]/div/div[1]/text()")
         if len(adress) > 0:
             adress = adress[0].strip()
         else:
@@ -52,22 +48,49 @@ class dou_parse:
         
 if __name__ == '__main__':
     
-    u = dou_parse('harkov')
+    city = {}
+    cities = list(csv.reader(open('cities.csv'), delimiter=";"))
+    print "Hi!"
+    while True:
+        print "Choose city, please. (\"Харьков\" is default, just press \"Enter\")"
+        for i, rec in enumerate(cities):
+            print i+1, "-", rec[1]
+        print "Enter 0 for exit"        
+        citynumber = raw_input ("\nEnter the number of city: ")
+        if len(citynumber) == 0: 
+            city['en'] = 'harkov'
+            city['ru'] = 'Харьков'
+            break
+        try:
+            citynumber = int(citynumber)
+        except:
+            print "Warning:", '"'+str(citynumber)+'"', "is not a correct number!!!\n"
+            continue
+        if citynumber == 0:
+            exit()
+        elif citynumber > 0 and citynumber <= len(cities):
+            city['en'] = cities[citynumber-1][0]
+            city['ru'] = cities[citynumber-1][1]
+            break
+        else:
+            print "Warning:", '"'+str(citynumber)+'"', "is not a correct number of city!!!\n"
+    
+    u = dou_parse(city)
     wb = Workbook()
     ws = wb.active
     ws.append(['Company', 'Staff', 'Offices', 'Adress', 'Site', 'Douurl'])
-    urllist = u.geturls('sitemap-companies.xml')
+    urllist = list(set(u.geturls('sitemap-companies-112.xml')))  # Change filename to yours jobs.dou.ua/sitemap-companies.xml
     urllistlen = len(urllist)
     starttime = datetime.now()
-    print 'Go!'
+    print "Go!"
+    print "Loking for offices in", city['ru']
     for i, url in enumerate(urllist):
         result = u.grabinfo(url)
         if result is not None:
             ws.append(result)
             wb.save('results.xlsx')
-        print 'Progres: ', i + 1,' out of ', urllistlen,'| url: ',  url
-    print '\nDone!'
+        print "Progres: ", i + 1," out of ", urllistlen,"| url: ",  url
+    print "\nDone!"
     print "Parser strted at: ", starttime.strftime("%Y-%m-%d %H:%M:%S")
     print "Finished at:      ", datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     print "Elapsed time:     ", datetime.now() - starttime
-    
